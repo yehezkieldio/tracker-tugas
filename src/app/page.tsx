568399c854f51c3894ea8b8f863a64d06389e273
elizielx <yehezkieldio35@gmail.com>
2023-10-21 03:09:48 +0800

    init: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main>
+            <div className="mx-4 my-4">
+                <Button variant="default">Hello, world</Button>
+            </div>
+        </main>
+    );
+}
